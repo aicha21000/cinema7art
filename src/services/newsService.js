006e9a4bd88b3c09d4da2c3799a3d6267e582b46
aicha21000 <salhi.aicha@hotmail.com>
2025-01-01 15:31:42 +0100

    Update site

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -2,7 +2,7 @@ import { fetchJson } from '../utils/api';
 import { NEWS_JSON_PATH } from '../config';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 class NewsService {
     constructor() {
